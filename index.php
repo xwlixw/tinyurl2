9a1ed0d4ba6bd6197acede0cbbcf00a15ec9405b
xwlixw <xwlixw@gmail.com>
2016-07-22 19:36:03 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<form action="create.php" method="post">
+URL: <input type="text" name="url_input"> 
+<input type="submit" value="shorten ->">
+</form>
+
+<form action="redirect.php" method="get">
+TinyURL: <input type="text" name="tinyurl"> 
+<input type="submit" value="redirect ->">
+</form>
+
+
+</body>
+</html>
